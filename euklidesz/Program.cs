14cd67596523a8be9eb6bd0d58ca2a2709c8897f
Varga Zsombor <61274133+vsumpi@users.noreply.github.com>
2024-10-06 17:09:34 +0200

    Add project files.

---
+++ euklidesz/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace euklidesz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add meg az egyik számot.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add meg a másik számot.");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int r;
+
+            Console.WriteLine($"Keressük meg {a} és {b} legnagyobb közös osztóját!");
+
+            while (b != 0)
+            {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+
+            Console.WriteLine($"A legnagyobb közös osztó: {a}");
+            Console.ReadKey();
+        }
+    }
+}
